b0dc34b4050f33a390ef8d8591a600d3bf0b2178
Pedro Souza <pedro.henrique@bs2tecnologia.com.br>
2021-06-06 02:45:10 -0300

    asd

---
+++ Random.Questions/Migrations/20210605193451_m1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project.Random.Questions.Migrations
+{
+    public partial class m1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IdQuestionNoReply",
+                table: "Reply",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
